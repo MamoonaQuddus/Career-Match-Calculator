9572b8c0132266effd9ad9268c575ec4e46f9051
Mamoona <mamoonaquddus@gmail.com>
2024-12-30 09:39:57 -0800

    added result-popup for pop-up reslut

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 window.onload= function(){
     let button = document.getElementById("calculate");
     button.addEventListener("click", calculateCareer);
-}
+    
+    let closePopup = document.getElementById("close-popup");
+    closePopup.addEventListener("click", () => {
+        document.getElementById("result-popup").style.display = "none";
+    });
 
-function calculateCareer(){
+    // Close the popup when clicking outside of the content
+    window.addEventListener("click", (event) => {
+        let popup = document.getElementById("result-popup");
+        if (event.target === popup) {
+            popup.style.display = "none";
+        }
+    });
+};
+
+function calculateCareer() {
     let yourName = document.getElementById("your-name").value;
     let friendName = document.getElementById("Friend-name").value;
-     
-    if (yourName != "" && friendName != "") {
-        let percentage = Math.floor(Math.random() * 101); //0-.99999
-        document.getElementById("result-message").innerText = `${yourName} and ${friendName}'s chances of making a great startup duo in tech!`;
-        document.getElementById("result-percentage").innerText = `${percentage}%`;
 
-      
-    }
-    else {
+    if (yourName !== "" && friendName !== "") {
+        let percentage = Math.floor(Math.random() * 101); // Generate a random percentage
+
+        // Update popup content
+        document.getElementById("popup-message").innerText = `${yourName} and ${friendName}'s chances of making a great startup duo in tech!`;
+        document.getElementById("popup-percentage").innerText = `${percentage}%`;
+
+        // Show the popup
+        document.getElementById("result-popup").style.display = "flex";
+    } else {
         alert("Please enter both names!");
     }
-        
-        
 }
+
